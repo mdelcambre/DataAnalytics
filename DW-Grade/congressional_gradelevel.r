e326fef5af1bbb41abe7e2075ef8e83c47813855
Mark Delcambre <mark@delcambre.com>
2014-06-10 22:07:30 -0700

    fixed url varriable

--- DW-Grade/congressional_gradelevel.r
+++ DW-Grade/congressional_gradelevel.r
@@ -12,14 +12,14 @@ library(RColorBrewer)
 library(scales)
 
 
-# Download the relevant Seattle Public Data into the data directory
+# Download the relevant Sunlight CongressionalPublic Data into the data directory
 if(!file.exists("data")){
     dir.create("data")
 }
 file_url <- "https://data.sunlightlabs.com/api/views/iwhe-qaqu/rows.csv?accessType=DOWNLOAD"
 file_loc <- "./data/Members_And_Grade_Level.csv"
 if(!file.exists(file_loc)){
-    download.file(file_location,destfile=file_loc,method="curl")
+    download.file(file_url,destfile=file_loc,method="curl")
     cat("## Date Downloaded", as.character(Sys.Date()),'\n',file=file_loc,append=T)
 }
 
@@ -30,4 +30,4 @@ if(!file.exists("output")){
 
 # Read the file into memory, last I checked this file is about 200 MB. May need to start subsampling
 # Then remove the NA columns for lat lon.
-data <- read.csv(file=file_loc, header=T, comment.char="#")
+#data <- read.csv(file=file_loc, header=T, comment.char="#")
