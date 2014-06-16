96eb7c38ca2062517ebb885f422de849347433ae
Mark Delcambre <mark@delcambre.com>
2014-06-16 16:24:37 -0700

    Fixed some bugs.
    Added strwrap.
    Adjusted plotting parameters.

--- Seattle_911/Seattle_911_Police_Map.r
+++ Seattle_911/Seattle_911_Police_Map.r
@@ -15,6 +15,9 @@ library(RgoogleMaps)
 library(RColorBrewer)
 library(scales)
 
+# Title wrapping helper function 
+titlewrap <- function(x, ...) paste(strwrap(x, ...), collapse = "\n")
+
 
 # Download the relevant Seattle Public Data into the data directory
 if(!file.exists("data")){
@@ -33,17 +36,26 @@ if(!file.exists("output")){
 }
 
 # Read the file into memory, last I checked this file is about 200 MB. May need to start subsampling
-# Then remove the NA columns for lat lon.
-data <- read.csv(file=file_loc, header=T, comment.char="#")
+data <- read.csv(file=file_loc, header=T, comment.char="#", na.strings = "NULL", skipNul=T, stringsAsFactors=F)
+
+# Clean up the dataset, remove leading and trailing white space and remove empty location data.
+data[["Event.Clearance.Group"]] <- as.factor(gsub("^\\s+|\\s+$", "",data[["Event.Clearance.Group"]]))
 data <- data[complete.cases(data["Latitude"],data["Longitude"]),]
-# data <- data[data["Latitude"] > 47.5545 & data["Latitude"]<47.69270 & data["Longitude"] > -122.4376 & data["Longitude"] < -122.2216,]
 
 # Center for the Google Map
 center <- sapply(c(data["Latitude"],data["Longitude"]),mean)
 
+# Setup the Google map centered on all data.
+map <- GetMap(center=center, SCALE=2, zoom=11)
+
+# Hard coded limits to make all of the kernel densities cover the same area.
+lims <- matrix(c(range(data$Latitude),range(data$Longitude)),nrow=2)
+lims <- unlist(LatLon2XY.centered(map, lims[,1],lims[,2]))
+
+
 # Setup Colors from Green to Red.
 # Add transparency
-colors <- rev(colorRampPalette(brewer.pal(11, 'RdYlGn'))(500))
+colors <- rev(colorRampPalette(brewer.pal(8, 'RdYlGn'))(16))
 colors_a <-alpha(colors,0.5)
 
 # Loop through each of the crime categories. Create a heatmap for each one and save them as a new png file.
@@ -54,25 +66,28 @@ for (crime in levels(data[["Event.Clearance.Group"]])){
     
 
     if (length(data_temp$Longitude)==0) { next } # if no values, skip no next
+    if (crime == "" | crime == "NULL") {next } # skip the two null sets
+
 
     # Open file png file, escape name, ideal for unix machines, will still break on windows.
-    crime = gsub("([/])","", crime)
+    crime = gsub("([/])","-", crime)
     png(paste("output/Seattle Police 911_",crime,".png",sep=""), width = 1280, height = 1280)
     
-    # Setup the Google map centered on all data.
-    map <- GetMap(center=center, SCALE=2, zoom=11)
     
     # Transform Cords for Google Map Plotting
     coords <- LatLon2XY.centered(map, data_temp$Latitude, data_temp$Longitude)
     coords <- data.frame(coords)
 
     # Generate the kernel density map for the heatmap
-    kd <- kde2d(coords$newX, coords$newY, n=1000)
+    kd <- kde2d(coords$newX, coords$newY, n=500, lims=lims)
 
     # Plot the map, heatmap, and points
     PlotOnStaticMap(map)
-    image(kd, col=colors_a, add=T)
     points(coords$newX, coords$newY, pch=16, cex=0.3)
+    image(kd, col=colors_a, add=T)
+
+    title(main=titlewrap(crime,20), cex.main=3, line=-85.5, col.main="black")
+    title(main=paste("n =",length(data_temp$Longitude)), cex.main=0.75, line=-86.5)
 
     # Save and close out the file
     dev.off()
