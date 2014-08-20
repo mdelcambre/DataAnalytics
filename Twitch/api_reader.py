1640ade2329be892a99c8be8e70920b36ad4f75f
Mark Delcambre <mark@delcambre.com>
2014-08-19 22:42:03 -0700

    touch further development, still messing around with API

--- Twitch/api_reader.py
+++ Twitch/api_reader.py
@@ -2,14 +2,29 @@
 """Reads the Twitch API"""
 
 
-from private_info import *
 import json
-from requests_oauthlib import OAuth2Session
+import requests
+from sets import Set
 
 # Global API url
 url = 'https://api.twitch.tv/kraken'
+header = {"Accept":"application/vnd.twitchtv.v2+json","Client-ID":r'nxdcczxegqz6miyg9b5br2nav2col3c'}
 
 
-twitch_api = json.loads(requests.get(url=url).text)
+twitch_api = json.loads(requests.get(url=url,headers=header).text)
 
-users = json.loads(requests.get(url=twitch_api['_links']['channel']).text)
+
+streams = Set()
+
+responses = []
+responses.append(json.loads(requests.get(url=twitch_api['_links']['streams']+'?limit=100',headers=header).text))
+for i in range(0,10):
+    responses.append(json.loads(requests.get(url=responses[i]['_links']['next'],headers=header).text))
+for response in responses:
+    for channel in response['streams']:
+        stream = channel['channel']['name']
+        if stream not in streams:
+            streams.add(stream)
+
+print(streams)
+print(len(streams))
